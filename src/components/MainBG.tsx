687e286eadcb253793fca02912777c915b2cfb12
canasmh <canasmh@yahoo.com>
2023-04-17 21:06:00 -0400

    Create a MainBG component that will be used as the 'main section' for each of the pages.

---
+++ src/components/MainBG.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const MainBG = styled.section`
+    height: 100vh;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("./breaking_bad_bg.jpeg");
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #CCCCCC;
+`
+export default MainBG;
