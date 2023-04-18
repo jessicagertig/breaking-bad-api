54b0e741c0aca15de422c8c9c56eb3caf5f84b2e
canasmh <canasmh@yahoo.com>
2023-04-17 21:11:42 -0400

    Made the Home Page. Does not have any state or user interaction implemented yet. Just the base UI. Also non-responsive as of now

---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import MainBG from "@/components/MainBG";
+import MainHeader, { SecondaryHeader, HeaderContainer } from "@/components/Header";
+import { Button, ButtonGroup } from "@/components/Buttons";
+import EndPointContainer, { Base, EndPoint } from "@/components/EndPoint";
+import FetchAPI from "@/components/FetchAPI";
+
+export default function Home(): JSX.Element {
+    return (
+        <>
+            <Head>
+                <title>Breaking Bad API | Home</title>
+            </Head>
+            <MainBG>
+                <HeaderContainer>
+                    <SecondaryHeader>
+                        <span>Th</span>e <span>Un</span>official 
+                    </SecondaryHeader>
+                    <MainHeader>
+                        <span>Br</span>eaking <span>Ba</span>d 
+                    </MainHeader>
+                    <SecondaryHeader>
+                        API 
+                    </SecondaryHeader>
+                </HeaderContainer>
+                <p style={{marginTop: "4.8rem", fontSize: "1.6rem"}}>Including <em>Better Call Saul</em> and <em>El Camino</em>!</p>
+                <ButtonGroup style={{margin: "5.6rem auto"}}>
+                    <Button>Characters</Button>
+                    <Button>Episodes</Button>
+                    <Button>Quotes</Button>
+                    <Button>Deaths</Button>
+                </ButtonGroup>
+                <EndPointContainer>
+                    <Base>Base URL</Base>
+                    <EndPoint>characters/random</EndPoint>
+                </EndPointContainer>
+
+                <FetchAPI style={{marginTop: "7.2rem"}}>
+                    Fetch Random Character
+                </FetchAPI>
+            </MainBG>
+        </>
+    )
+}
