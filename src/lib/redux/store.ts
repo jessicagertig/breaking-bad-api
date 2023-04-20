fbaca414a66caf1ed2293ba61706acd5713ea0ed
Jessica Cooper <jessicagcooper@gmail.com>
2023-04-20 18:42:10 -0500

    Add type declarations to redux store.

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,7 +1,13 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-export default configureStore({
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+export const store=configureStore({
   reducer: {
     // Add your reducers here
   }
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppDispatch: () => typeof store.dispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
