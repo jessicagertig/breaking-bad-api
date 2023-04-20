788d22240cbaa947c82d1e1f5af81bd94e5cf558
Jessica Cooper <jessicagcooper@gmail.com>
2023-04-20 18:41:41 -0500

    Add basic redux store to App.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,15 @@ import Head from 'next/head';
 import Nav from '@/components/Navigation';
 import { AppProps } from 'next/app';
 import '../styles/global.css';
+import { Provider } from 'react-redux';
 
+import reduxStore from '@/lib/redux/store';
 // The _app component file is rendered with every page. It serves as
 // kind of like a template for all the other pages. 
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <Provider store={reduxStore}>
             <Head>
                 <style>
                     @import url('https://fonts.googleapis.com/css2?family=Anton&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
@@ -23,8 +25,8 @@ function MyApp({ Component, pageProps }: AppProps) {
             </Head>
             <Nav />
             <Component {...pageProps} />
-        </>
-);
+        </Provider>
+    );
 }
 
 export default MyApp;
