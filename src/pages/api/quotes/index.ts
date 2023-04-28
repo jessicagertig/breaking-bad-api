e948e0df02a842273550916e23e97963420d43b3
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-04-27 21:40:05 -0400

    added random quote by character and production query and added quote by character

--- src/pages/api/quotes/index.ts
+++ src/pages/api/quotes/index.ts
@@ -1,9 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
-const data = require("../../../../data/quotes.json");
+const quoteData = require("../../../../data/quotes.json");
+
+/**
+ *
+ * @param req /api/quotes?character=Jesse+Pinkman ==> fetches all quotes by specific character
+ * @param req /api/quotes ====> fetches all quotes
+ * @param res
+ */
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { character } = req.query;
   try {
-    res.status(200).json(data);
+    if (character) {
+      const filterQuoteByCharacter = quoteData.filter(
+        (quote: any) => quote.character === character
+      );
+      if (filterQuoteByCharacter) {
+        res.status(200).json(filterQuoteByCharacter);
+      } else {
+        res.status(400).json({ message: "Name of character not Found" });
+      }
+    } else {
+      res.status(200).json(quoteData);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
