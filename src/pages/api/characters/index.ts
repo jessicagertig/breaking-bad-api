d3353998d019c21d796972a196a272f542da75b3
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-04-27 20:57:25 -0400

    added query by name

--- src/pages/api/characters/index.ts
+++ src/pages/api/characters/index.ts
@@ -3,11 +3,17 @@ const charactersData = require("../../../../data/characters.json");
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { production } = req.query;
+  const { name } = req.query;
   if (production) {
     const filteredByProductionCharacters = charactersData.filter(
       (character: any) => character.production === production
     );
     res.status(200).json(filteredByProductionCharacters);
+  } else if (name) {
+    const filterByCharacterName = charactersData.filter(
+      (character: any) => character.name === name
+    );
+    res.status(200).json(filterByCharacterName);
   } else {
     res.status(200).json(charactersData);
   }
