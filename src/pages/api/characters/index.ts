5efab6fab2ff34e0cd1fbae5cfa459b49dc23826
Miguel A. Ledesma <miguelledesma33@gmail.com>
2023-04-27 20:53:21 -0400

    filter by production added to index file

--- src/pages/api/characters/index.ts
+++ src/pages/api/characters/index.ts
@@ -2,5 +2,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 const charactersData = require("../../../../data/characters.json");
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(charactersData);
+  const { production } = req.query;
+  if (production) {
+    const filteredByProductionCharacters = charactersData.filter(
+      (character: any) => character.production === production
+    );
+    res.status(200).json(filteredByProductionCharacters);
+  } else {
+    res.status(200).json(charactersData);
+  }
 }
