1fcc0995188d02a2dbe92aec0337545ac2bca473
canasmh <canasmh@yahoo.com>
2023-04-30 12:57:12 -0400

    Make the width of the EndPoint component a percentage, even on large ( > 1200px ) screens

--- src/components/EndPoint.tsx
+++ src/components/EndPoint.tsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 // this is the div below the buttons that shows the current 
 // endpoint according to what button the user clicks.
 const EndPointContainer = styled.div`
-
+    width: 30%;
+    text-align: center;
     @media only screen and (max-width: 1200px) {
         width: 60%;
         text-align: center;
@@ -16,11 +17,7 @@ const EndPointContainer = styled.div`
     }
     & > div {
         display: inline-block;
-        padding: 1.6rem 3.2rem;
-        @media only screen and (max-width: 1200px) {
-            padding: 2rem 0;
-            text-align: center;
-        }
+        padding: 1.6rem 0;
         @media only screen and (max-width: 600px) {
             padding: 1.6rem 0;
             text-align: center;
@@ -33,10 +30,7 @@ const EndPointContainer = styled.div`
 const Base = styled.div`
     background-color: #00A86B;
     border-radius: 30px 0 0 30px;
-
-    @media only screen and (max-width: 1200px) {
-        width: 35%;
-    }
+    width: 35%;
 `
 
 // The "Endpoint" part of that above described component
@@ -44,10 +38,7 @@ const EndPoint = styled.div`
     background-color: #EDEDED;
     color: #333333;
     border-radius: 0 30px 30px 0;
-
-    @media only screen and (max-width: 1200px) {
-        width: 63%;
-    }
+    width: 63%;
 `
 
 export default EndPointContainer;
