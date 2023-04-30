aae04d236096feb11bbcdbf7284e12fdb612b53f
canasmh <canasmh@yahoo.com>
2023-04-30 13:01:33 -0400

    Dynamically display the current api random route based on the buttons that the user clicks. Make an axios call when user clicks on Fetch Random... but only logs it to the console as of right now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,18 @@ import MainHeader, { SecondaryHeader, HeaderContainer } from "@/components/Heade
 import { Button, ButtonGroup } from "@/components/Buttons";
 import EndPointContainer, { Base, EndPoint } from "@/components/EndPoint";
 import FetchAPI from "@/components/FetchAPI";
+import { useState } from "react";
+import axios from "axios";
 
 // This is the component for the "/" route
 export default function Home(): JSX.Element {
+
+    const [endPoint, setEndPoint] = useState("characters")
+
+    const fetchRandom = async () => {
+        const response = await axios.get(`/api/${endPoint}/random`)
+        console.log(response.data);
+    };
 
     return (
         <>
@@ -27,18 +36,18 @@ export default function Home(): JSX.Element {
                 </HeaderContainer>
                 <p className="gilliverse">Including <em>Better Call Saul</em> and <em>El Camino</em>!</p>
                 <ButtonGroup>
-                    <Button>Characters</Button>
-                    <Button>Episodes</Button>
-                    <Button>Quotes</Button>
-                    <Button>Deaths</Button>
+                    <Button onClick={() => setEndPoint('characters')} isActive={endPoint === 'characters'}>Characters</Button>
+                    <Button onClick={() => setEndPoint('episodes')} isActive={endPoint === 'episodes'}>Episodes</Button>
+                    <Button onClick={() => setEndPoint('quotes')} isActive={endPoint === 'quotes'}>Quotes</Button>
+                    <Button onClick={() => setEndPoint('deaths')} isActive={endPoint === 'deaths'}>Deaths</Button>
                 </ButtonGroup>
                 <EndPointContainer>
                     <Base>Base URL</Base>
-                    <EndPoint>characters/random</EndPoint>
+                    <EndPoint>{endPoint}/random</EndPoint>
                 </EndPointContainer>
 
-                <FetchAPI>
-                    Fetch Random Character
+                <FetchAPI onClick={fetchRandom}>
+                    Fetch Random {endPoint[0].toUpperCase() + endPoint.slice(1, endPoint.length -1)}
                 </FetchAPI>
             </MainBG>
         </>
