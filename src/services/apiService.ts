ce1046b80f1168da9e3b1bf1db1dbfd6866778cd
Jessica Cooper <jessicagcooper@gmail.com>
2023-05-02 19:07:18 -0500

    Change hardcoded url to env variable.

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { Character, Death, Episode, Quote } from '../types';
+import { Character, Death, Episode, Quote } from '@/types';
 import { HYDRATE } from 'next-redux-wrapper';
 
 export const api = createApi({
   reducerPath: 'apiService',
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   extractRehydrationInfo(action, { reducerPath }) {
     if (action.type === HYDRATE) {
       return action.payload[reducerPath]
