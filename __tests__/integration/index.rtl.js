502c5b51b20c3815588b24a1bce93f62d8b387be
Jessica Cooper <jessicagcooper@gmail.com>
2023-05-26 16:58:53 -0500

    More complete first unit test & begin integration test.

--- __tests__/integration/index.rtl.js
+++ __tests__/integration/index.rtl.js
@@ -2,8 +2,10 @@ import { render, screen } from '@testing-library/react';
 import Home from '@/pages/index';
 import userEvent from '@testing-library/user-event';
 
-test('displays default endpoint', () => {
-  const char = {name: 'characters', endpoint: 'characters/random', buttonText: 'Fetch Random Characters'}
+test('correct child component display on click of fetch random', async () => {
   render(<Home />)
   expect(screen.getByText(char.endpoint)).toBeInTheDocument()
+  expect(screen.getByText(char.buttonText)).toBeInTheDocument()
+  
+  await userEvent.click(screen.getByText(char.buttonText))
 })
